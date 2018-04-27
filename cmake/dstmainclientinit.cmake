d19e27262460bd0ff98c32138c8ef60507465a18
Calvin Rose <calsrose@gmail.com>
2018-04-27 01:20:39 -0400

    Make CMake not depend on location of build folder.

--- cmake/dstmainclientinit.cmake
+++ cmake/dstmainclientinit.cmake
@@ -1,9 +1,10 @@
 # Include bin2h cmake code
-set (CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../cmake")
+set (CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+
 include(bin2h)
 
 bin2h (
-    SOURCE_FILE ${CMAKE_CURRENT_SOURCE_DIR}/../src/mainclient/init.dst
+    SOURCE_FILE ${CMAKE_CURRENT_LIST_DIR}/../src/mainclient/init.dst
     HEADER_FILE "clientinit.gen.h"
     VARIABLE_NAME dst_mainclient_init
 )
--- cmake/dststlbootstrap.cmake
+++ cmake/dststlbootstrap.cmake
@@ -1,9 +1,10 @@
 # Include bin2h cmake code
-set (CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../cmake")
+set (CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+
 include(bin2h)
 
 bin2h (
-    SOURCE_FILE ${CMAKE_CURRENT_SOURCE_DIR}/../src/compiler/boot.dst
+    SOURCE_FILE ${CMAKE_CURRENT_LIST_DIR}/../src/compiler/boot.dst
     HEADER_FILE "dststlbootstrap.gen.h"
     VARIABLE_NAME dst_stl_bootstrap_gen
 )
