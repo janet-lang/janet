af557ecbd48490ffe256ceab56f31149bcf9198e
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-28 16:54:44 +0100

    Add the parameter to file/fdopen

--- src/core/io.c
+++ src/core/io.c
@@ -165,11 +165,11 @@ static Janet cfun_io_fopen(int32_t argc, Janet *argv) {
 }
 
 static Janet cfun_io_fdopen(int32_t argc, Janet *argv) {
-    janet_arity(argc, 1, 2);
+    janet_arity(argc, 1, 3);
     const int fd = janet_getinteger(argv, 0);
     const uint8_t *fmode;
     int flags;
-    if (argc == 2) {
+    if (argc >= 2) {
         fmode = janet_getkeyword(argv, 1);
         flags = checkflags(fmode);
     } else {
@@ -180,7 +180,19 @@ static Janet cfun_io_fdopen(int32_t argc, Janet *argv) {
 #define fdopen _fdopen
 #endif
     FILE *f = fdopen(fd, (const char *)fmode);
-    return f ? makef(f, flags) : janet_wrap_nil();
+	
+    if(NULL == f) {
+        return janet_wrap_nil();
+    } else {
+        if(argc == 3) {
+            JanetBuffer * buffer = janet_getbuffer(argv, 2);
+            
+            if((buffer->capacity == 0 && setvbuf(f, NULL, _IONBF, 0)) || setvbuf(f, buffer->data, _IOFBF, buffer->capacity))
+                return janet_wrap_nil();
+        }
+        
+        return makef(f, flags);
+    }
 }
 
 static Janet cfun_io_fileno(int32_t argc, Janet *argv) {
@@ -599,11 +611,15 @@ static const JanetReg io_cfuns[] = {
     },
     {
         "file/fdopen", cfun_io_fdopen,
-        JDOC("(file/fdopen fd &opt mode)\n\n"
+        JDOC("(file/fdopen fd &opt mode &opt buf)\n\n"
              "Create a file from an fd. fd is a platform specific file descriptor, and "
              "mode is a set of flags indicating the mode to open the file in. "
-             "mode is a keyword where each character represents a flag. If the file "
-             "cannot be opened, returns nil, otherwise returns the new file handle. "
+             "mode is a keyword where each character represents a flag."
+			 "buf is the buffer instance to be used for output caching."
+             "if buf's capacity is equal to zero, buffering is disabled."
+             "the buffer shouldn't be resized after the operation completes."
+			 "If the file cannot be opened, returns nil, otherwise returns the new file"
+			 "handle. "
              "Mode flags:\n\n"
              "\tr - allow reading from the file\n"
              "\tw - allow writing to the file\n"
