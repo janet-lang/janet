e202d3083594f678931c834e8e7dc1f1f80c480f
Calvin Rose <calsrose@gmail.com>
2020-04-17 13:39:23 -0500

    Use make format.

--- test/fuzzers/fuzz_dostring.c
+++ test/fuzzers/fuzz_dostring.c
@@ -2,21 +2,21 @@
 #include <string.h>
 #include <janet.h>
 
-int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size){
-	char *new_str = (char *)malloc(size+1);
-	if (new_str == NULL){
-		return 0;
-	}
-	memcpy(new_str, data, size);
-	new_str[size] = '\0';
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    char *new_str = (char *)malloc(size + 1);
+    if (new_str == NULL) {
+        return 0;
+    }
+    memcpy(new_str, data, size);
+    new_str[size] = '\0';
 
-	/* janet logic */
-	janet_init();
-	JanetTable *env = janet_core_env(NULL);
-	janet_dostring(env, new_str, "main", NULL);
-	janet_deinit();
+    /* janet logic */
+    janet_init();
+    JanetTable *env = janet_core_env(NULL);
+    janet_dostring(env, new_str, "main", NULL);
+    janet_deinit();
 
-	free(new_str);
-	return 0;
+    free(new_str);
+    return 0;
 }
 
