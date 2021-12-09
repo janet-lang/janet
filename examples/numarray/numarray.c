f586a8a9dc2a9d326f70776d507c0cbbed14788b
Josef Pospíšil <josef.pospisil@laststar.eu>
2021-12-09 11:18:05 +0100

    Add length to method and lib fn to numarray

--- examples/numarray/numarray.c
+++ examples/numarray/numarray.c
@@ -76,9 +76,16 @@ void num_array_put(void *p, Janet key, Janet value) {
     }
 }
 
+static Janet num_array_length(int32_t argc, Janet *argv) {
+    janet_fixarity(argc, 1);
+    num_array *array = (num_array *)janet_getabstract(argv, 0, &num_array_type);
+    return janet_wrap_number(array->size);
+}
+
 static const JanetMethod methods[] = {
     {"scale", num_array_scale},
     {"sum", num_array_sum},
+    {"length", num_array_length},
     {NULL, NULL}
 };
 
