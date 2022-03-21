3715d7a1840a4c58b665bf28b49933603f3e416a
Calvin Rose <calsrose@gmail.com>
2022-03-21 18:22:59 -0500

    Auto update copyright date.

--- src/core/state.c
+++ src/core/state.c
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2021 Calvin Rose
+* Copyright (c) 2022 Calvin Rose
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to
