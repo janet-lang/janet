7fda7709ff15ab4b4cdea57619365eb2798f15c4
rick2600 <ricks2600@gmail.com>
2022-05-02 12:57:47 -0300

    fix negative count passed to cfun_array_new_filled

--- src/core/array.c
+++ src/core/array.c
@@ -137,6 +137,7 @@ JANET_CORE_FN(cfun_array_new_filled,
               "Creates a new array of `count` elements, all set to `value`, which defaults to nil. Returns the new array.") {
     janet_arity(argc, 1, 2);
     int32_t count = janet_getinteger(argv, 0);
+    if (count < 0) janet_panic("expected positive integer");
     Janet x = (argc == 2) ? argv[1] : janet_wrap_nil();
     JanetArray *array = janet_array(count);
     for (int32_t i = 0; i < count; i++) {
