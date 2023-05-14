398833ebe333efa751c52d2fa0f0a940d1d9878b
Calvin Rose <calsrose@gmail.com>
2023-05-14 09:18:54 -0500

    Enable FFI module unconditionally.

--- src/conf/janetconf.h
+++ src/conf/janetconf.h
@@ -6,8 +6,8 @@
 #define JANET_VERSION_MAJOR 1
 #define JANET_VERSION_MINOR 28
 #define JANET_VERSION_PATCH 0
-#define JANET_VERSION_EXTRA ""
-#define JANET_VERSION "1.28.0"
+#define JANET_VERSION_EXTRA "-dev"
+#define JANET_VERSION "1.28.0-dev"
 
 /* #define JANET_BUILD "local" */
 
