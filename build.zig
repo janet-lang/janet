5fe1597d5a958aaf5f6ec3f51082e137889e517c
jpaquim <joaopaquim@gmail.com>
2025-10-10 00:11:39 +0100

    Minimal janet_boot build

--- build.zig
+++ build.zig
@@ -1,8 +1,78 @@
 const std = @import("std");
 
+const core_sources = [_][]const u8{
+    "src/core/abstract.c",
+    "src/core/array.c",
+    "src/core/asm.c",
+    "src/core/buffer.c",
+    "src/core/bytecode.c",
+    "src/core/capi.c",
+    "src/core/cfuns.c",
+    "src/core/compile.c",
+    "src/core/corelib.c",
+    "src/core/debug.c",
+    "src/core/emit.c",
+    "src/core/ev.c",
+    "src/core/ffi.c",
+    "src/core/fiber.c",
+    "src/core/filewatch.c",
+    "src/core/gc.c",
+    "src/core/inttypes.c",
+    "src/core/io.c",
+    "src/core/marsh.c",
+    "src/core/math.c",
+    "src/core/net.c",
+    "src/core/os.c",
+    "src/core/parse.c",
+    "src/core/peg.c",
+    "src/core/pp.c",
+    "src/core/regalloc.c",
+    "src/core/run.c",
+    "src/core/specials.c",
+    "src/core/state.c",
+    "src/core/string.c",
+    "src/core/strtod.c",
+    "src/core/struct.c",
+    "src/core/symcache.c",
+    "src/core/table.c",
+    "src/core/tuple.c",
+    "src/core/util.c",
+    "src/core/value.c",
+    "src/core/vector.c",
+    "src/core/vm.c",
+    "src/core/wrap.c",
+};
+
+const boot_sources = [_][]const u8{
+    "src/boot/array_test.c",
+    "src/boot/boot.c",
+    "src/boot/buffer_test.c",
+    "src/boot/number_test.c",
+    "src/boot/system_test.c",
+    "src/boot/table_test.c",
+};
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const janet_boot_mod = b.addModule("janet_boot", .{
+        .optimize = optimize,
+        .target = target,
+    });
+    const version = "\"local\"";
+    janet_boot_mod.addCSourceFiles(.{
+        .files = &(core_sources ++ boot_sources),
+        .flags = &.{ "-DJANET_BOOTSTRAP", b.fmt("-DJANET_BUILD={s}", .{version})},
+    });
+    janet_boot_mod.addIncludePath(b.path("src/include"));
+    janet_boot_mod.addIncludePath(b.path("src/conf"));
+
+    const janet_boot_exe = b.addExecutable(.{
+        .name = "janet_boot",
+        .root_module = janet_boot_mod,
+    });
+    b.installArtifact(janet_boot_exe);
 
     const mod = b.addModule("janet", .{
         .root_source_file = b.path("src/root.zig"),
