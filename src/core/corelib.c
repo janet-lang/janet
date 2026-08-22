06f4a80c7f670f502ed1dba9c311d781f7e01068
sogaiu <983021772@users.noreply.github.com>
2026-08-22 19:51:42 +0900

    Improve docstring for describe

--- src/core/corelib.c
+++ src/core/corelib.c
@@ -317,10 +317,10 @@ JANET_CORE_FN(janet_core_native,
 
 JANET_CORE_FN(janet_core_describe,
               "(describe x)",
-              "Returns a string that is a human-readable description of `x`. "
-              "For recursive data structures, the string returned contains a "
-              "pointer value from which the identity of `x` "
-              "can be determined.") {
+              "Returns a string that is a human-readable description of "
+              "`x`. For indexed, dictionary, fiber, pointer, and some "
+              "abstract types, the string returned contains a pointer "
+              "value from which the identity of `x` can be determined.") {
     JanetBuffer *b = janet_buffer(0);
     for (int32_t i = 0; i < argc; ++i)
         janet_description_b(b, argv[i]);
